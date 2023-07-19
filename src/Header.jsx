0e8bebaaa5efe3e155fd975e1b9c0dd526c756b1
croyal <croyal@comcast.net>
2023-07-19 11:23:22 -0400

    add images and update text

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import RMPLogo from '../src/images/RMP_3-01.jpg';
 
 const Header = () => {
     return (
-        <div>
-            <h1><center>Royal Consulting LLC</center></h1>
+        <div width="100%">
+            <img  src={RMPLogo} alt="Royal Consulting Logo" width="800" height="300" />
             <hr />
         </div>
     );
